bf8c8cf2ba1a84c95986d4cd1e8388968f06059b
Anirudh-rao <anirudh131299@gmail.com>
2023-09-10 16:40:28 +0530

    Control Flow -2
    For Loops, Nested Loop , While Loop ,  Break and Continue

---
+++ 2.ControlFlow/ForLoop.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let mut sum  = 0;
+
+    for i in 1..11{
+        sum += i;
+    }
+    println!("Sum is  {}", sum);
+}
---
+++ 2.ControlFlow/Loop.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let mut number  =0;
+    
+    // Infinite Loop Start Here
+    loop {
+        number += 1;
+        println!("{}", number);
+        if number >= 10{
+            //Exit the Loop
+            break;
+        }
+    }
+}
