ca741a5821ab6909220d8b59da3029d2ae7e03d3
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2023-11-14 15:52:09 +0530

    Modules and Package in Rust
    Modules and Packages in Rust

---
+++ 7.ModulesAndPackages/NestedModules.rs
@@ -0,0 +1,16 @@
+// nested module
+pub mod player {
+    pub mod sprite {
+        pub fn create() {
+            println!("called player::sprite::create");
+        }
+    }
+}
+
+// bring the create function into scope
+use player::sprite::create;
+
+fn main() {
+    // call public function directly
+    create();
+}
