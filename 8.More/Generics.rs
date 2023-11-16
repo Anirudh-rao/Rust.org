2750b1201dba7c608c842d00107b373c0eea7e0f
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2023-11-16 19:03:57 +0530

    More
    Matching , Generic and Generic Functions in Rust

---
+++ 8.More/Generics.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // defining a struct with generic data type
+    #[derive(Debug)]
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+
+    // initializing a generic struct with i32 data type
+    let int_point = Point { x: 1, y: 2 };
+
+    // initializing a generic struct with f32 data type
+    let float_point = Point { x: 1.1, y: 2.2 };
+
+    println!("int_point: {:?}", int_point);
+    println!("float_point: {:?}", float_point);
+}
