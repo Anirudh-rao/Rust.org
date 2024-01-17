e1daa9b5975c5f185883a5650cd369fb4f1118f9
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2024-01-17 18:43:52 +0530

    Conditonals
    For Loop and Iterators

---
+++ 8.Flow of Control/ForLoopIterator.rs
@@ -0,0 +1,28 @@
+fn main(){
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            // TODO ^ Try deleting the & and matching just "Ferris"
+            _ => println!("Hello {}", name),
+        }
+    }
+    println!("names: {:?}", names);
+
+    // into_iter
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    // iter_mut 
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+    
+}
