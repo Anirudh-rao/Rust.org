00ca16824024006e39ea27eff8a8377becb790c3
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2024-02-27 18:35:48 +0530

    HelloWorld in Rust

---
+++ 1.Introduction/HelloWorld.rs
@@ -0,0 +1,11 @@
+fn main() {
+    
+    
+    // To Do Marco
+    // Display the message "Hello, world!"
+    todo!("Display the message by using the println!() macro");
+    println!("Hello, world!");
+
+    // Call println! with three arguments: a string, a value, a value
+    println!("The first letter of the English alphabet is {} and the last letter is {}.", 'A', 'Z');
+}
